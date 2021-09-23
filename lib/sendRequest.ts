78ee24b71fd1bcd31a2037fb66ef9cdbfb5f8e1a
ykyk3 <sakura198606@gmail.com>
2021-09-23 16:43:58 +0900

    add send request

--- lib/sendRequest.ts
+++ lib/sendRequest.ts
@@ -1,14 +1,12 @@
+import fetch from 'node-fetch';
+
 const makeParameters = (options: Object) =>
     Object.entries(options)
         .map((option) => option.join('='))
         .join('&');
 
 type Options = string | string[] | boolean | undefined;
-const get = (
-    url: string,
-    options?: Record<string, Options>,
-    init?: RequestInit
-) => {
+const get = (url: string, options?: Record<string, Options>, init?: any) => {
     const parameters = options ? makeParameters(options) : undefined;
     const requestUrl = `${url}${parameters ? `?${parameters}` : ''}`;
     return fetch(requestUrl, { ...(init || {}), method: 'GET' });
