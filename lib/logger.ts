a16ed9c9b358999f7df26ab13e9e5dfae6ba443d
ykyk3 <sakura198606@gmail.com>
2021-09-23 16:43:39 +0900

    add logger

--- lib/logger.ts
+++ lib/logger.ts
@@ -5,14 +5,16 @@ enum Color {
     Yellow = '\x1b[33m',
     Blue = '\x1b[34m',
 }
+const reset = '\u001b[0m';
+const format = (str: string, color: Color) => `${color}${str}${reset}`;
 function info(str: string): void {
-    console.log(`${Color.Blue}[Log]: ${str}`);
+    console.log(format(str, Color.Blue));
 }
 function error(str: string): void {
-    console.error(`${Color.Red}[Error]: ${str}`);
+    console.error(format(str, Color.Red));
 }
 function warning(str: string): void {
-    console.warn(`${Color.Yellow}[Warning]: ${str}`);
+    console.warn(format(str, Color.Yellow));
 }
 
 const logger = {
