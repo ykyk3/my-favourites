96acbec3d839821606c6ff9650337877e7b939f9
ykyk3 <sakura198606@gmail.com>
2021-09-25 10:08:01 +0900

    webpackコンフィグを修正
    webpackconfigのコミットもれ

---
+++ webpack.config.dev.js
@@ -0,0 +1,18 @@
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        historyApiFallback: true,
+        inline: true,
+        open: true,
+        host: 'localhost',
+        port: 8080,
+        proxy: {
+            '/api/**': {
+                target: 'http://localhost:3000',
+                secure: false,
+                logLevel: 'debug',
+            },
+        },
+    },
+};
+module.exports = devConfig;
--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,15 @@
-const path = require('path');
-module.exports = {
-    mode: 'development', // production or development
-    entry: './client/index.tsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js',
-        publicPath: '/',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-            },
-            {
-                // 対象となるファイルの拡張子(scss)
-                test: /\.scss$/,
-                // Sassファイルの読み込みとコンパイル
-                use: [
-                    // CSSファイルを抽出するように MiniCssExtractPlugin のローダーを指定
-                    //   {
-                    //     loader: MiniCssExtractPlugin.loader,
-                    //   },
-                    // CSSをバンドルするためのローダー
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            //URL の解決を無効に
-                            url: false,
-                            // ソースマップを有効に
-                            sourceMap: true,
-                        },
-                    },
-                    // Sass を CSS へ変換するローダー
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            // dart-sass を優先
-                            implementation: require('sass'),
-                            sassOptions: {
-                                // fibers を使わない場合は以下で false を指定
-                                // fiber: require('fibers'),
-                                fiber: false,
-                            },
-                            // ソースマップを有効に
-                            sourceMap: true,
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-    // module: { // Loader setting See: https://webpack.js.org/concepts/#loaders
-    //     rules: [{ test: /\.txt$/, use: 'raw-loader' }],
-    // },
-    // import 文で.ts ファイルを解決する。
-    resolve: { extensions: ['.ts', '.tsx', '.js', '.json'] },
-    target: ['web', 'es5'],
+const commonConfig = require('./webpack.config.common');
+const { merge } = require('webpack-merge');
+
+module.exports = (env, args) => {
+    switch (args.mode) {
+        case 'development':
+            const developmentConfig = require('./webpack.config.dev');
+            return merge(commonConfig, developmentConfig);
+        case 'production':
+            const productionConfig = require('./webpack.config.production');
+            return merge(commonConfig, productionConfig);
+        default:
+            throw new Error('No matching configuration was found!');
+    }
 };
